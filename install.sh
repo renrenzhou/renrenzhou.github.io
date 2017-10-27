5679ade54b357ac04fc75dc55b0ad41ab7fa8227
renrenzhou <itjustzhou@gmail.com>
2017-10-27 23:09:35 +0800

    c change

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@ npm install
 rm -rf themes
 mkdir themes
 git clone https://github.com/iissnan/hexo-theme-next themes/next
+mv -f themes/next/_config.yml bak_theme.yml
 cp -f theme.yml themes/next/_config.yml
